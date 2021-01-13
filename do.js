95ab52f8f7332148f9473f8d4576792a1d2b062d
Yashika832 <76933714+Yashika832@users.noreply.github.com>
2021-01-13 17:23:23 +0530

    Add files via upload

---
+++ do.js
@@ -0,0 +1,25 @@
+document.querySelector("#taskAdd").addEventListener("click", function () {
+    //storing input task in variable
+    let task = document.querySelector("#taskInput").value;
+    //removing input
+    document.querySelector("#taskInput").value = "";
+    let entry = $('<li class="item"><input type="checkbox" class="checkBox"><span class="itemText">' + task + '</span><input type="text" class="editBox"><button class="edit" >Edit</button><button class="delete">Delete</button></li>');
+    $("#assignedTask").append(entry);
+    $(".checkBox").click(function(){
+        if($(this).is(":checked")){
+            $("#completedTask").append($(this).parent(".item"))
+        }else {
+            $("#assignedTask").append($(this).parent(".item"))
+        }  
+    });
+    $(".edit").click(function(){
+        if($(this).parent(".item").find(".editBox").val()== ""){
+            $(this).prev().val($(this).parent(".item").find(".itemText").text());
+        }else{
+            $(this).parent(".item").find(".itemText").text($(this).prev().val());
+        }
+    });
+    $(".delete").click(function(){
+        $(this).parent('.item').remove();
+    });
+});
